174fc359d21fd838b8ff752e01ccffacfc87d88e
Leo <truongvietduc@gmail.com>
2019-11-20 14:24:51 +0700

    Initialization

---
+++ MealPlan/DAL/StudentDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MealPlan.DAL.Entities;
+namespace MealPlan.DAL
+{
+    public class StudentDbContext : DbContext
+    {
+        public StudentDbContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
