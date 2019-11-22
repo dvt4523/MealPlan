45847925f7f8bcdb3f83d56186dbf4182c7e2fc2
Leo Tr <truongvietduc@gmail.com>
2019-11-22 13:19:07 +0700

    [DB01] RawFood DB
    Added RawFood DB and Bootstrap js & css files.

---
+++ MealPlan/Models/RawFoodModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MealPlan.Models
+{
+    public class RawFoodModel
+    {
+        public int Calories { get; set; }
+        public int Protein { get; set; }
+        public int Fat { get; set; }
+        public int Fiber { get; set; }
+        public enum FoodTag { Meat, Carb, Veggie, Fruit, };
+    }
+}
