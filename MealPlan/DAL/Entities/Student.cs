b883b01c1e6288db5d0d6bfe68e6ee098f8880ef
Nguyễn Lê Minh <nguyenleminh530447@gmail.com>
2019-11-25 11:15:42 +0700

    Clean code & fix

--- MealPlan/DAL/Entities/Student.cs
+++ MealPlan/DAL/Entities/Student.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MealPlan.DAL.Entities
 {
     public class Student
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
